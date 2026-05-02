f862af686e6e4262479d97eb1f40216ce0e8612b
siff6 <hashifjade907@gmail.com>
2026-05-02 19:35:13 +0700

    FIX

---
+++ my-project/app/api/todos/route.js
@@ -0,0 +1,16 @@
+import connectDB from '@/lib/mongodb';
+import Todo from '@/models/Todo'; 
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  await connectDB();
+  const todos = await Todo.find({});
+  return NextResponse.json(todos);
+}
+
+export async function POST(request) {
+  await connectDB();
+  const body = await request.json();
+  const newTodo = await Todo.create(body);
+  return NextResponse.json(newTodo, { status: 201 });
+}
---
+++ my-project/lib/mongodb.js
@@ -0,0 +1,9 @@
+// lib/mongodb.js
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(process.env.MONGO_URI);
+};
+
+export default connectDB;
